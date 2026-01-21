20c3bed6e7cd32aa2ada7cdfdda0d14aa2a23b1c
venkatesh <perupoguvenkatesh5073@gmail.com>
2026-01-21 14:33:22 +0530

    updation

---
+++ src/components/DataGrid.stories.tsx
@@ -0,0 +1,12 @@
+import {DataGrid} from "./DataGrid.tsx";
+import type { RowData } from "./DataGrid";
+
+export default {
+  title: "Components/DataGrid",
+  component: DataGrid,
+};
+export const Primary={
+    args:{
+        label:"Click Me",
+    }
+}
